500b79cbd9ab379a072346a19fbd732095c7e551
Shanthibeth <shanthir@mit.edu>
2014-06-18 10:10:38 -0400

    app skeleton

---
+++ first_app/public/javascripts/first.js
@@ -0,0 +1,25 @@
+//first.js
+
+var $ = require("jquery")
+var d3 = require("d3");
+
+$(document).ready(function(){
+
+
+document.getElementById("title").className = "hidden"
+
+var sampleSVG = d3.select("#viz")
+        .append("svg")
+        .attr("width", 100)
+        .attr("height", 100);    
+
+    sampleSVG.append("circle")
+        .style("stroke", "gray")
+        .style("fill", "white")
+        .attr("r", 40)
+        .attr("cx", 50)
+        .attr("cy", 50)
+        .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+        .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+    })
